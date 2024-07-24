a1940d5019a84f97d0460ce0bedc37764c5b7dd8
abhii08 <abhinavsharma392@gmail.com>
2024-07-24 09:17:14 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://abhinavsharma392:q2mp6ENksrrMG4HH@cluster0.siez3ev.mongodb.net/todos")
+mongoose.connect("mongodb+srv://_________________@cluster0.siez3ev.mongodb.net/todos")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
